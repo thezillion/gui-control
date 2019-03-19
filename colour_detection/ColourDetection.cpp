15bc86a3c714200f62eea6170e7d49a2f53c947b
Prince Mathew <i.princemathew@gmail.com>
2019-03-19 15:06:21 +0530

    Removed unwated header files, removed using namespace xyz

--- colour_detection/ColourDetection.cpp
+++ colour_detection/ColourDetection.cpp
@@ -4,12 +4,9 @@
 
 #include "include/init.hpp"
 
-using namespace cv;
-using namespace std;
-
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
+    cv::VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
--- colour_detection/src/color_detection.cpp
+++ colour_detection/src/color_detection.cpp
@@ -1,71 +1,62 @@
 #include <opencv2/opencv.hpp>
-#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include "../include/color_detection.hpp"
 
-using namespace cv;
-using namespace std;
-
 void ColorDetectionModule::thresh_callback() {
+    
     /** @function thresh_callback */
-    Mat src_copy = colour.clone();
-    Mat threshold_output;
-    Mat moms = Mat::zeros( src_copy.size(), CV_8UC3 );
-    vector<vector<Point> > contours;
-    vector<vector<Point> > contours1;
-    vector<Vec4i> hierarchy;
+    cv::Mat src_copy = colour.clone();
+    cv::Mat threshold_output;
+    cv::Mat moms = cv::Mat::zeros( src_copy.size(), CV_8UC3 );
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<std::vector<cv::Point> > contours1;
+    std::vector<cv::Vec4i> hierarchy;
 
     /// Detect edges using Threshold
-    threshold( src_copy, threshold_output, thresh, 255, THRESH_BINARY );
+    cv::threshold( src_copy, threshold_output, thresh, 255, THRESH_BINARY );
 
 
     /// Find contours
-    findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    cv::findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
     // find moments of the image
 
     //found the moments for each contour differently and considered only those with area >100
-    vector<vector<Point>> hull( contours.size() );
-    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-    Mat drawing2 = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    std::vector<std::vector<cv::Point>> hull( contours.size() );
+    cv::Mat drawing = cv::Mat::zeros( threshold_output.size(), CV_8UC3 );
+    cv::Mat drawing2 = cv::Mat::zeros( threshold_output.size(), CV_8UC3 );
     for(int i=0; i< contours.size(); i++){
-        Moments m = moments(contours[i], true);
+        cv::Moments m = moments(contours[i], true);
         if(m.m00<100) continue;
-        Point p(m.m10/m.m00, m.m01/m.m00);
-
+        cv::Point p(m.m10/m.m00, m.m01/m.m00);
         
-        // coordinates of centroid
-        // cout<< Mat(p)<< endl;
-        
-        // show the image with a point mark at the centroid
-        circle(moms, p, 5, Scalar(255, 255, 255), -1);
+        // show the image with a cv::Point mark at the centroid
+        cv::circle(moms, p, 5, cv::Scalar(255, 255, 255), -1);
 
 
-        convexHull( Mat(contours[i]), hull[i], false );
+        convexHull( cv::Mat(contours[i]), hull[i], false );
 
-        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-        fillConvexPoly( drawing2, contours[i], 255);
+        cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        cv::drawContours( drawing, hull, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+        cv::fillConvexPoly( drawing2, contours[i], 255);
     }
 
-    imshow( "Hull demo", drawing );
-    imshow( "Points", moms);
-    imshow( "Polygons", drawing2);
+    cv::imshow( "Hull demo", drawing );
+    cv::imshow( "Points", moms);
+    cv::imshow( "Polygons", drawing2);
 
-    findContours( threshold_output, contours1, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-    vector<vector<Point>> hull1( contours1.size() );
-    Mat drawing3 = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    cv::findContours( threshold_output, contours1, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    std::vector<std::vector<cv::Point>> hull1( contours1.size() );
+    cv::Mat drawing3 = cv::Mat::zeros( threshold_output.size(), CV_8UC3 );
     for(int i=0; i< contours1.size(); i++){
-        Moments m = moments(contours1[i], true);
-        if(m.m00<100) continue;
-        Point p(m.m10/m.m00, m.m01/m.m00);
-
+        cv::Moments m = moments(contours1[i], true);
         
-        // coordinates of centroid
-        // cout<< Mat(p)<< endl;
+        if(m.m00 < 100) continue;
         
-        // show the image with a point mark at the centroid
-        circle(drawing3, p, 5, Scalar(255, 255, 255), -1);
+        cv::Point p(m.m10/m.m00, m.m01/m.m00);
+        
+        // show the image with a cv::Point mark at the centroid
+        cv::circle(drawing3, p, 5, cv::Scalar(255, 255, 255), -1);
     }
-    imshow( "New Points", drawing3);
+    cv::imshow( "New Points", drawing3);
 }
